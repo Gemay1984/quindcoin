8f0c732853c0e21b1fb395903f6dd190a882abf3
Gemay1984 <gasv04@hotmail.com>
2026-04-10 09:43:27 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'quindcoin-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  'https://i.postimg.cc/9fJsDW4Z/escudo.png',
+  'https://i.postimg.cc/Vs3XK67H/Imagen1.jpg',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request);
+      })
+  );
+});
